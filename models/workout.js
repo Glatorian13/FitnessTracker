f628719405e000e43c8226bf70a5bab554d3b915
Glatorian13 <glatorian13@gmail.com>
2021-04-11 12:10:34 -0400

    typo fix, fix server.js, need to work on api routes, html works

--- models/workout.js
+++ models/workout.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const { Schema } = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 //this file is written based off the javascript in public folder, so it was quite easy to reverse engineer from that.
 
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev")); //this is required for some reason??
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //serve static stuff
 app.use(express.static("public"));
 //compress for speed
-app.use(compression());
+//app.use(compression());
 
 //connect to heroku or local
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
@@ -21,9 +23,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useUnifiedTopology: true,
 });
 
+const db = require("./models");
+
 //routes  WORK ON THIS
-app.use(require("./routes/api-route.js"));
-app.use(require("./routes/html-route"));
+//app.use(require('./routes/api-route.js'));
+app.use(require('./routes/html-route.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
