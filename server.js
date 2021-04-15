ac004bdfd495f017fb449a8db7f93c63f7aa3d96
Glatorian13 <69689219+Glatorian13@users.noreply.github.com>
2021-04-14 22:17:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 //app.use(compression());
 
 //connect to heroku or local
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
